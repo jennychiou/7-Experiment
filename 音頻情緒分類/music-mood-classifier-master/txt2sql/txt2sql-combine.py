2ae43e2a8dbc5ef6fb5c27130d38672e69037627
Jenny Chiou <01xa14@ms.mingdao.edu.tw>
2019-04-15 23:26:05 +0800

    0415 歌單新增至2000首

--- 音頻情緒分類/music-mood-classifier-master/txt2sql/txt2sql-combine.py
+++ 音頻情緒分類/music-mood-classifier-master/txt2sql/txt2sql-combine.py
@@ -3,10 +3,10 @@ titledata2=[]
 singerdata=[] #singer
 singerdata2=[]
         
-with open('txtFile/combine(2000)0415-song.txt','r') as f:
+with open('combine(2000)0415-song.txt','r') as f:
         for line in f:
             titledata.append(line.strip('\n').split(','))
-with open('txtFile/combine(2000)0415-singer.txt','r') as f:
+with open('combine(2000)0415-singer.txt','r') as f:
         for line in f:
             singerdata.append(line.strip('\n').split(','))
 
@@ -23,13 +23,14 @@ for i in range(len(titledata)):
 ##(1, '1979', 'Smashing Pumpkins'),
     titlen = titledata2[i]
     aname = singerdata2[i]
-    print(str('(') + str(i+1) + str(', ') + str(titlen) + str(', ') + str(aname) + str('),'))
+##    print(str('(') + str(i+1) + str(', ') + str(titlen) + str(', ') + str(aname) + str('),'))
+print("SQL Import done !")
 
 #import to database
 import  pymysql
 conn  =  pymysql.connect ( host = '127.0.0.1' ,  user = 'root' ,  passwd = "850121" ,  db = 'musicdb' ) 
 cur  =  conn.cursor ()
-sql = "INSERT INTO songlist (song, singer, lyrics, vector) VALUES (%s, %s,%s,%s)" 
+sql = "INSERT INTO songlist_combine (song, singer, lyrics, vector) VALUES (%s,%s,%s,%s)" 
 for i in range(len(singerdata)):
     titlen = titledata2[i]
     aname = singerdata2[i]
